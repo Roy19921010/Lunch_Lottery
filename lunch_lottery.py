b55e91398def68ba6265934d56df0222ea672aae
Jingxiong Liu <jingxoing.liu@volvocars.com>
2019-01-23 11:54:10 +0100

    removed residue parts

--- lunch_lottery.py
+++ lunch_lottery.py
@@ -2,7 +2,7 @@ import requests
 import re
 import random
 import matplotlib.pyplot as plt
-r = requests.get('https://www.strawpoll.me/16900109/r')
+r = requests.get('https://www.strawpoll.me/17209027/r')
 #print(r.text)
 search_pattern=re.compile('<p class="option-text"><span>(.*)</span> <span class="option-count" data-count=(.*)>(.*)</span></p>')
 places_votes = re.findall(search_pattern, r.text)
@@ -19,17 +19,7 @@ for i in range(len(probs)):
     temp=temp+float(probs[i])
     portion.append(temp)
 portion=[0]+portion
-#print(portion)
-#if ran_num<float(probs[0]):
-#    result=places_votes[0][0]
-#elif ran_num<(float(probs[0])+float(probs[1])):
-#    result=places_votes[1][0]
-#elif ran_num<float(probs[0])+float(probs[1])+float(probs[2]):
-#    result=places_votes[2][0]
-#elif ran_num<float(probs[0])+float(probs[1])+float(probs[2])+float(probs[3]):
-#    result=places_votes[3][0]
-#else:
-#    result=places_votes[4][0]
+
 def less_than_50_result():
 	print('*'*40)
 	print('Based on the {} votes we get this time'.format(total_votes))
@@ -47,7 +37,6 @@ def more_than_50_result():
 	print('*'*40)
 	for i in range(len(probs)):
 	    print('Probability for going to {} is {}'.format(places_votes[i][0],probs[i]))
-	#print(probs)
 	print('*'*40)
 	print("Since the vote for {} is more than 50%, let's follow majority then".format(result))
 	print('*'*40)
@@ -56,8 +45,6 @@ def more_than_50_result():
 	print('*'*40)
 	print(r"Let's have lunch in {} this time!!!".format(result))
 #print(probs)
-#print(range(len(portion)))
-#print(probs[2])
 for i in range(len(portion)):
     if probs[i]>0.5:
         result=places_votes[i][0]
@@ -69,16 +56,5 @@ for i in range(len(portion)):
        break
     else:
         continue
-#if ran_num<portion[0]:
-#    result=places_votes[0][0]
-#elif ran_num<portion[1]:
-#    result=places_votes[1][0]
-#elif ran_num<portion[2]:
-#    result=places_votes[2][0]
-#elif ran_num<portion[3]:
-#    result=places_votes[3][0]
-#else:
-#    result=places_votes[4][0]
-#print(total_votes)
-#print(ran_num)
+
 
